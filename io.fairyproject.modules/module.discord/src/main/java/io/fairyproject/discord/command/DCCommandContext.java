07b4f107fb475ba25bf111f93ab6bbfab6a1381f
LeeGod <lee20040919@gmail.com>
2022-01-18 13:48:38 +0700

    Use DCMessageChannel in CommandContext

--- io.fairyproject.modules/module.discord/src/main/java/io/fairyproject/discord/command/DCCommandContext.java
+++ io.fairyproject.modules/module.discord/src/main/java/io/fairyproject/discord/command/DCCommandContext.java
@@ -2,21 +2,21 @@ package io.fairyproject.discord.command;
 
 import io.fairyproject.command.CommandContext;
 import io.fairyproject.discord.DCBot;
+import io.fairyproject.discord.channel.DCMessageChannel;
 import lombok.Getter;
 import lombok.Setter;
-import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.entities.User;
 
 @Getter
 public class DCCommandContext extends CommandContext {
 
     private final DCBot bot;
-    private final MessageChannel channel;
+    private final DCMessageChannel channel;
     private final User author;
     @Setter
     private String commandPrefix = DCBot.DEFAULT_COMMAND_PREFIX;
 
-    public DCCommandContext(String[] args, DCBot bot, MessageChannel channel, User author) {
+    public DCCommandContext(String[] args, DCBot bot, DCMessageChannel channel, User author) {
         super(args);
         this.bot = bot;
         this.channel = channel;
