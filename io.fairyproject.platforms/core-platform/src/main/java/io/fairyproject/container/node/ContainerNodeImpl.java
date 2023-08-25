0117fb0d5576eaddf1c1cab4cc5c027a320ec810
LeeGodSRC <lee20040919@gmail.com>
2023-08-25 18:32:03 +0800

    fix: Fixed code style

--- io.fairyproject.platforms/core-platform/src/main/java/io/fairyproject/container/node/ContainerNodeImpl.java
+++ io.fairyproject.platforms/core-platform/src/main/java/io/fairyproject/container/node/ContainerNodeImpl.java
@@ -113,22 +113,30 @@ public class ContainerNodeImpl implements ContainerNode {
         if (this.isResolved())
             return this;
 
+        boolean success = true;
         for (ContainerObj obj : this.objects.values()) {
+            if (!success)
+                break;
+
             for (ContainerObj.DependEntry entry : obj.dependEntries()) {
                 final Class<?> dependClass = entry.getDependClass();
 
-                if (ContainerReference.hasObj(dependClass))
-                    continue;
-
-                ContainerLogger.report(this, obj, null,
-                        "Unknown dependency: " + dependClass.getName(),
-                        " ",
-                        "Maybe you forgot to register it? Make sure the dependency is marked as @InjectableComponent",
-                        "and you have the class in the classpath.");
-                return this;
+                if (!ContainerReference.hasObj(dependClass)) {
+                    ContainerLogger.report(this, obj, null,
+                            "Unknown dependency: " + dependClass.getName(),
+                            " ",
+                            "Maybe you forgot to register it? Make sure the dependency is marked as @InjectableComponent",
+                            "and you have the class in the classpath.");
+                    success = false;
+                    break;
+                }
             }
             this.graph.add(obj);
         }
+
+        if (!success)
+            return this;
+
         this.graph.resolve();
 
         for (ContainerNode childNode : this.childNodes) {
