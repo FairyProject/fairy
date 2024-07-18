02052389065fc61a20deb09c6386a47ee7068617
LeeGodSRC <lee20040919@gmail.com>
2024-07-18 18:04:08 +0800

    chore: Use unchecked cast for MetaKey casting chore: Improved bukkit logger by using console logger

--- framework/platforms/bukkit-platform/src/main/java/io/fairyproject/bukkit/logger/Log4jLogger.java
+++ framework/platforms/bukkit-platform/src/main/java/io/fairyproject/bukkit/logger/Log4jLogger.java
@@ -4,63 +4,66 @@ import io.fairyproject.log.ILogger;
 import org.apache.logging.log4j.LogManager;
 
 public class Log4jLogger implements ILogger {
+    
+    private final org.apache.logging.log4j.Logger logger = LogManager.getLogger("Fairy");
+    
     @Override
     public void info(String message, Object... replace) {
-        LogManager.getLogger().info(String.format(message, replace));
+        logger.info(String.format(message, replace));
     }
 
     @Override
     public void debug(String message, Object... replace) {
-        LogManager.getLogger().debug(String.format(message, replace));
+        logger.debug(String.format(message, replace));
     }
 
     @Override
     public void warn(String message, Object... replace) {
-        LogManager.getLogger().warn(String.format(message, replace));
+        logger.warn(String.format(message, replace));
     }
 
     @Override
     public void error(String message, Object... replace) {
-        LogManager.getLogger().error(String.format(message, replace));
+        logger.error(String.format(message, replace));
     }
 
     @Override
     public void info(String message, Throwable throwable, Object... replace) {
-        LogManager.getLogger().info(String.format(message, replace), throwable);
+        logger.info(String.format(message, replace), throwable);
     }
 
     @Override
     public void debug(String message, Throwable throwable, Object... replace) {
-        LogManager.getLogger().debug(String.format(message, replace), throwable);
+        logger.debug(String.format(message, replace), throwable);
     }
 
     @Override
     public void warn(String message, Throwable throwable, Object... replace) {
-        LogManager.getLogger().warn(String.format(message, replace), throwable);
+        logger.warn(String.format(message, replace), throwable);
     }
 
     @Override
     public void error(String message, Throwable throwable, Object... replace) {
-        LogManager.getLogger().error(String.format(message, replace), throwable);
+        logger.error(String.format(message, replace), throwable);
     }
 
     @Override
     public void info(Throwable throwable) {
-        LogManager.getLogger().info(throwable);
+        logger.info(throwable);
     }
 
     @Override
     public void debug(Throwable throwable) {
-        LogManager.getLogger().debug(throwable);
+        logger.debug(throwable);
     }
 
     @Override
     public void warn(Throwable throwable) {
-        LogManager.getLogger().warn(throwable);
+        logger.warn(throwable);
     }
 
     @Override
     public void error(Throwable throwable) {
-        LogManager.getLogger().error(throwable);
+        logger.error(throwable);
     }
 }
