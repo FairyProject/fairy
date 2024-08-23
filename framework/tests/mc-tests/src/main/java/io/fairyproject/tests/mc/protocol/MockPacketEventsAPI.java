1bd22782f2f334574fc61756f404e5539a3a2650
LeeGodSRC <lee20040919@gmail.com>
2024-08-23 22:49:10 +0800

    fix: Fixed MockPacketEventsAPI

--- framework/tests/mc-tests/src/main/java/io/fairyproject/tests/mc/protocol/MockPacketEventsAPI.java
+++ framework/tests/mc-tests/src/main/java/io/fairyproject/tests/mc/protocol/MockPacketEventsAPI.java
@@ -72,6 +72,11 @@ public class MockPacketEventsAPI extends PacketEventsAPI<Plugin> {
     }
 
     @Override
+    public boolean isTerminated() {
+        return false;
+    }
+
+    @Override
     public Plugin getPlugin() {
         return FairyPlatform.INSTANCE.getMainPlugin();
     }
