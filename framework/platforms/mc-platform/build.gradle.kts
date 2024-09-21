c06cf804691d86263c3bfd043c1b196a38dcf61d
LeeGodSRC <lee20040919@gmail.com>
2024-09-21 15:11:46 +0800

    chore: Bumped version to 0.7.9b1-SNAPSHOT chore: Bumped packetevents to 2.5.0 stable

--- framework/platforms/mc-platform/build.gradle.kts
+++ framework/platforms/mc-platform/build.gradle.kts
@@ -15,7 +15,7 @@ dependencies {
     api("net.kyori:adventure-text-serializer-gson-legacy-impl:4.17.0")
     api("net.kyori:adventure-text-serializer-plain:4.17.0")
 
-    api("com.github.retrooper:packetevents-api:2.5.0-SNAPSHOT") {
+    api("com.github.retrooper:packetevents-api:2.5.0") {
         exclude(group = "net.kyori")
     }
 
