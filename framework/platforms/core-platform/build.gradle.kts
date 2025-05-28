dfa4c504665fdc7d9793cc9c54313e6ce0d994e3
LeeGodSRC <lee20040919@gmail.com>
2025-05-28 12:44:25 +0800

    chore: Bumped version to 0.8b4-SNAPSHOT chore: Bumped jvm-driver to 9.9.10

--- framework/platforms/core-platform/build.gradle.kts
+++ framework/platforms/core-platform/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 dependencies {
     api("io.github.classgraph:classgraph:4.8.173")
-    api("org.burningwave:jvm-driver:8.16.1")
+    api("io.github.toolfactory:jvm-driver:9.9.10")
     api("javax.annotation:javax.annotation-api:1.3.2")
     api("javax.persistence:javax.persistence-api:2.2")
     api("com.google.code.gson:gson:2.9.1")
