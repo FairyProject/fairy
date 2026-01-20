738f0bffb92f1c1c0e466dae6b2c13a956c635ef
LeeGodSRC <lee20040919@gmail.com>
2026-01-20 17:45:45 +0800

    feat: Added hytale test plugin

---
+++ test-hytale-plugin/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    `java`
+    id("io.fairyproject")
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+}
+
+fairy {
+    name.set("test-hytale-plugin")
+    mainPackage.set("io.example.hytale")
+    fairyPackage.set("io.fairyproject")
+}
+
+dependencies {
+    implementation("io.fairyproject:hytale-bootstrap")
+    implementation("io.fairyproject:hytale-platform")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+runHytaleServer {  }
