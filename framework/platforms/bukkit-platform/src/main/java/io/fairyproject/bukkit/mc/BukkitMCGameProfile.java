8544520279f25247da7d5e32af74298f337f60a2
LeeGodSRC <lee20040919@gmail.com>
2026-04-10 00:19:21 +0800

    fix: improve compatibility with authlib API by using reflection for GameProfile methods

--- framework/platforms/bukkit-platform/src/main/java/io/fairyproject/bukkit/mc/BukkitMCGameProfile.java
+++ framework/platforms/bukkit-platform/src/main/java/io/fairyproject/bukkit/mc/BukkitMCGameProfile.java
@@ -9,9 +9,57 @@ import io.fairyproject.util.EquivalentConverter;
 import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nonnull;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 import java.util.*;
 
 public class BukkitMCGameProfile implements MCGameProfile {
+
+    // Reflection cache for authlib API compatibility (old getter-style vs new record-style)
+    private static final Method GP_GET_NAME;
+    private static final Method GP_GET_ID;
+    private static final Method GP_GET_PROPERTIES;
+    private static final Method PROP_GET_NAME;
+    private static final Method PROP_GET_VALUE;
+    private static final Method PROP_GET_SIGNATURE;
+    private static final Constructor<?> PROP_CONSTRUCTOR;
+
+    static {
+        GP_GET_NAME = resolveMethod(GameProfile.class, "name", "getName");
+        GP_GET_ID = resolveMethod(GameProfile.class, "id", "getId");
+        GP_GET_PROPERTIES = resolveMethod(GameProfile.class, "properties", "getProperties");
+        PROP_GET_NAME = resolveMethod(com.mojang.authlib.properties.Property.class, "name", "getName");
+        PROP_GET_VALUE = resolveMethod(com.mojang.authlib.properties.Property.class, "value", "getValue");
+        PROP_GET_SIGNATURE = resolveMethod(com.mojang.authlib.properties.Property.class, "signature", "getSignature");
+        PROP_CONSTRUCTOR = resolvePropConstructor();
+    }
+
+    private static Method resolveMethod(Class<?> clazz, String... names) {
+        for (String name : names) {
+            try {
+                return clazz.getMethod(name);
+            } catch (NoSuchMethodException ignored) {
+            }
+        }
+        throw new IllegalStateException("No method found on " + clazz.getName() + " for names: " + Arrays.toString(names));
+    }
+
+    private static Constructor<?> resolvePropConstructor() {
+        try {
+            return com.mojang.authlib.properties.Property.class.getConstructor(String.class, String.class, String.class);
+        } catch (NoSuchMethodException e) {
+            throw new IllegalStateException("Cannot find Property constructor", e);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T> T invoke(Method method, Object obj) {
+        try {
+            return (T) method.invoke(obj);
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public static final EquivalentConverter<MCGameProfile> CONVERTER = new EquivalentConverter<MCGameProfile>() {
         @Override
@@ -46,12 +94,16 @@ public class BukkitMCGameProfile implements MCGameProfile {
 
     @Override
     public String getName() {
-        return this.gameProfile.getName();
+        return invoke(GP_GET_NAME, this.gameProfile);
     }
 
     @Override
     public UUID getUuid() {
-        return this.gameProfile.getId();
+        return invoke(GP_GET_ID, this.gameProfile);
+    }
+
+    private PropertyMap getPropertyMap() {
+        return invoke(GP_GET_PROPERTIES, this.gameProfile);
     }
 
     @NotNull
@@ -62,15 +114,19 @@ public class BukkitMCGameProfile implements MCGameProfile {
 
     @Override
     public boolean hasProperty(String property) {
-        return this.gameProfile.getProperties().containsKey(property);
+        return getPropertyMap().containsKey(property);
     }
 
     @Override
     public void setProperty(Property property) {
         String name = property.getName();
-        PropertyMap properties = this.gameProfile.getProperties();
+        PropertyMap properties = getPropertyMap();
         properties.removeAll(name);
-        properties.put(name, new com.mojang.authlib.properties.Property(name, property.getValue(), property.getSignature()));
+        try {
+            properties.put(name, (com.mojang.authlib.properties.Property) PROP_CONSTRUCTOR.newInstance(name, property.getValue(), property.getSignature()));
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
@@ -80,16 +136,20 @@ public class BukkitMCGameProfile implements MCGameProfile {
 
     @Override
     public void clearProperties() {
-        this.gameProfile.getProperties().clear();
+        getPropertyMap().clear();
     }
 
     @Override
     public boolean removeProperty(String property) {
-        return !this.gameProfile.getProperties().removeAll(property).isEmpty();
+        return !getPropertyMap().removeAll(property).isEmpty();
     }
 
     private static Property toBukkit(com.mojang.authlib.properties.Property property) {
-        return new Property(property.getName(), property.getValue(), property.getSignature());
+        return new Property(
+                invoke(PROP_GET_NAME, property),
+                invoke(PROP_GET_VALUE, property),
+                invoke(PROP_GET_SIGNATURE, property)
+        );
     }
 
     private class PropertySet extends AbstractSet<Property> {
@@ -97,12 +157,12 @@ public class BukkitMCGameProfile implements MCGameProfile {
         @Override
         @Nonnull
         public Iterator<Property> iterator() {
-            return new ProfilePropertyIterator(gameProfile.getProperties().values().iterator());
+            return new ProfilePropertyIterator(getPropertyMap().values().iterator());
         }
 
         @Override
         public int size() {
-            return gameProfile.getProperties().size();
+            return getPropertyMap().size();
         }
 
         @Override
@@ -120,7 +180,7 @@ public class BukkitMCGameProfile implements MCGameProfile {
 
         @Override
         public boolean contains(Object o) {
-            return o instanceof ProfileProperty && gameProfile.getProperties().containsKey(((ProfileProperty) o).getName());
+            return o instanceof ProfileProperty && getPropertyMap().containsKey(((ProfileProperty) o).getName());
         }
 
         private class ProfilePropertyIterator implements Iterator<Property> {
