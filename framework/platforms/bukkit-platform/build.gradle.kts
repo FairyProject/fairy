ec85a73e9ef326af891f826b84605824fc0ee9c0
LeeGodSRC <lee20040919@gmail.com>
2026-06-02 15:22:20 +0800

    fix: update packetevents dependencies to version 2.12.2

--- framework/platforms/bukkit-platform/build.gradle.kts
+++ framework/platforms/bukkit-platform/build.gradle.kts
@@ -6,7 +6,7 @@ dependencies {
     api(project(":mc-platform"))
     api("net.kyori:adventure-platform-bukkit:4.4.1")
     api("net.kyori:adventure-text-serializer-bungeecord:4.4.1")
-    api("com.github.retrooper:packetevents-spigot:2.12.1") {
+    api("com.github.retrooper:packetevents-spigot:2.12.2") {
         exclude(group = "net.kyori")
     }
     compileOnly("io.papermc.paper:paper-api:1.21.3-R0.1-SNAPSHOT") {
